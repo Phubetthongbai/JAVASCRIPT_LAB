aecdf76427f6f558fe887fb4b64e004292631c47
phubetthongbai <nicekup07@gmail.com>
2023-12-21 21:22:03 +0700

    new

---
+++ Assignment/Example1.js
@@ -0,0 +1,17 @@
+/*function PrintName(name) {
+    return ("Hello " + name + ", your name is " + name.length + " letters long");
+}
+
+// Example usage:
+//PrintName("phubet");
+//PrintName("nice");
+
+console.log(PrintName("phuber")); // Display "Hello Robert your name is 6 letters long"
+console.log(PrintName("nice"));  // Display "Hello Jo your name is 2 letters long"*/
+
+function helloWorld(name) {
+    return ("Hello " + name + ", your name is " + name.length + " letters long");
+}
+
+console.log(helloWorld("Robert")); // Display "Hello Robert your name is 6 letters long"
+console.log(helloWorld("Jo"));  // Display "Hello Jo your name is 2 letters long"
