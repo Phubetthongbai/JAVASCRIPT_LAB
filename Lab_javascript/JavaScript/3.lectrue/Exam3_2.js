7577c64d00e69e685f10b643cb92b0dc61f49adb
phubetthongbai <nicekup07@gmail.com>
2023-12-21 15:34:22 +0700

    new

--- Lab_javascript/JavaScript/3.lectrue/Exam3_2.js
+++ Lab_javascript/JavaScript/3.lectrue/Exam3_2.js
@@ -2,10 +2,19 @@ for (let char of ['Table','Chairs','Couch']){
     console.log(char);
 }
 
+/*
 const names = ['Table','Chairs','Couch'];
 for (let index = 0; index < names.length ; index++){
     const name = name[index];
     console.log(name);
 }
+*/
 
-//let arr1 = [,,,]
+/*
+function iterateArray(array) {
+    for (let char of array) {
+        console.log(char);
+    }
+}
+iterateArray(['Table','Chairs','Couch']);
+*/
